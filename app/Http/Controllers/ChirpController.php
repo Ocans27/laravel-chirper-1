6e413894075d4dad6dafc92b314ca8d1209adfd6
Kunti Najma Jalia <najmajalia02@gmail.com>
2024-10-30 01:05:01 +0700

    blade

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -4,7 +4,8 @@ namespace App\Http\Controllers;
  
 use App\Models\Chirp;
 use Illuminate\Http\Request; 
-use Illuminate\Http\Response; 
+use Illuminate\Http\Response;
+use Illuminate\View\View;
  
 class ChirpController extends Controller
 {
@@ -12,10 +13,10 @@ class ChirpController extends Controller
      * Display a listing of the resource.
      */
 
-    public function index(): Response 
+    public function index(): View 
     {
         //
-        return response('Assalamualaikum, Kunti Najma Jalia!');
+        return view('chirps.index');
     }
 
     /**
